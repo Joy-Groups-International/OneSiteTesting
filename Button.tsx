264bfa7393ddeba995939aeb422bfd47596f47dd
myohtut <myohtutlin.anbuchi@gmail.com>
2024-07-18 11:19:08 +0700

    first-commit

---
+++ Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+      <div className="bg-secondary-400 rounded-md cursor-pointer btn text-white border-none outline-none py-3 flex justify-center items-center w-[100%] mt-10">
+          Component Button
+      </div>
+    )
+  }
+  
+  export default Button;
---
+++ IndexTemplate.tsx
@@ -0,0 +1,12 @@
+import Button from './Button'
+
+const IndexTemplate = () => {
+  return (
+    <>
+    <div>IndexTemplate This is Git Testing</div>
+    <Button/>
+    </>
+  )
+}
+
+export default IndexTemplate
